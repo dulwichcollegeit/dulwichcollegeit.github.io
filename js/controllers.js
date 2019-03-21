f9f4dac74ec9d02c0f244f440f28cb4fee8199b5
Dulwich College IT <dulwichcollegeit@gmail.com>
2019-03-21 17:55:22 +0000

    test

--- js/controllers.js
+++ js/controllers.js
@@ -8,13 +8,13 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('homeCtrl', ['$scope', '$stateParams',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams', '$ionicPopup',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $ionicPopup) {
 
   $scope.src_function = function () { 
-    alert('Hello');
+    $ionicPopup.alert({title: 'Hello'});
   };
 
 
