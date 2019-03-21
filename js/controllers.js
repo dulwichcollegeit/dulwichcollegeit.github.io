145031e1e1b21655ffcca97f4b412810625797ad
Dulwich College IT <dulwichcollegeit@gmail.com>
2019-03-21 17:43:26 +0000

    cont

--- js/controllers.js
+++ js/controllers.js
@@ -8,10 +8,15 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+
+  $scope.src_function = function () { 
+    alert('Hello');
+  };
+
 
 
 }])
