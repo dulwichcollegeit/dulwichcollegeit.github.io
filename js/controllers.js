e5a72f4721d6f32b9646d6bb282a1cc2ae234d10
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2019-03-25 21:37:51 +0000

    test

--- js/controllers.js
+++ js/controllers.js
@@ -8,10 +8,10 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('homeCtrl', ['$scope', '$stateParams', '$ionicPopup', 'Form',   // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams', '$ionicPopup',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $ionicPopup, Form) {
+function ($scope, $stateParams, $ionicPopup) {
 
   
   $scope.src_function = function () { 
